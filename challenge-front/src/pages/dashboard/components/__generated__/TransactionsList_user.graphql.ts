ee15e2ee43ed8895bb8509b6d0a1485335993d44
WillMS28 <silva.miranda.william@gmail.com>
2024-09-03 18:32:07 -0300

    .

--- challenge-front/src/pages/dashboard/components/__generated__/TransactionsList_user.graphql.ts
+++ challenge-front/src/pages/dashboard/components/__generated__/TransactionsList_user.graphql.ts
@@ -32,7 +32,6 @@ export type TransactionsList_user$data = {
   readonly " $fragmentType": "TransactionsList_user";
 };
 export type TransactionsList_user$key = {
-  wallet: any;
   readonly " $data"?: TransactionsList_user$data;
   readonly " $fragmentSpreads": FragmentRefs<"TransactionsList_user">;
 };
